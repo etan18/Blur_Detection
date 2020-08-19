5bca3bce2cd1a831b62cd2493a1f8063f57379e7
aneeshakodati <68865291+aneeshakodati@users.noreply.github.com>
2020-08-19 16:05:30 -0400

    Update blur_detection.py

--- blur_detection.py
+++ blur_detection.py
@@ -9,15 +9,10 @@ def variance_of_laplacian(image):
 	# compute the Laplacian of the image and then return the focus
 	# measure, which is simply the variance of the Laplacian
 	return cv2.Laplacian(image, cv2.CV_64F).var()
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-
-threshold=100; #might need to change
-args = vars(ap.parse_args())
-
-#converts to gray
+        threshold=100; #might need to change
+        #converts to gray
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #variance of laplacian
+        #variance of laplacian
 	fm = variance_of_laplacian(gray)
 	text = "Not Blurry"
 	# if the focus measure is less than the supplied threshold,
