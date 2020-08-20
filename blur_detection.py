815037097d5eab3b608fa84d9d7ff6e18357ec7f
aneeshakodati <68865291+aneeshakodati@users.noreply.github.com>
2020-08-20 10:13:37 -0400

    Update blur_detection.py

--- blur_detection.py
+++ blur_detection.py
@@ -1,26 +1,26 @@
-# script for tuning parameters
 from imutils import paths
 import argparse
 import cv2
 
-#code to determine how blurry something is-- has two arguments, path to input directory of
+threshold=100
+
 #images and threshold for how blurry is really blurry
 def variance_of_laplacian(image):
-	# compute the Laplacian of the image and then return the focus
-	# measure, which is simply the variance of the Laplacian
-	return cv2.Laplacian(image, cv2.CV_64F).var()
-        threshold=100 #might need to change
+        #make constants start w/ capital(use pylint to make sure)
+        #might need to change, put in beginning of code/before function
         #converts to gray
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         #variance of laplacian
-	fm = variance_of_laplacian(gray)
+	blurriness = cv2.Laplacian(gray, cv2.CV_64F).var()#second partial derivative of x/y direction
 	text = "Not Blurry"
 	# if the focus measure is less than the supplied threshold,
 	# then the image should be considered "blurry"
-	if fm < threshold:
+	if bluriness < threshold:
 		text = "Blurry"
 	# show the image
-	cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
+	cv2.putText(image, "{}: {:.2f}".format(text, bluriness), (10, 30),
 		cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
 	cv2.imshow("Image", image)
 	key = cv2.waitKey(0)
+	return bluriness
+#how long does this take to do
