9497143a2da7315e729c35a9c2c6a0a9d09460db
etan18 <erintan4@gmail.com>
2020-08-24 09:37:55 -0400

    Merged functions and files into one file.
    Not complete - remaining error occurs when handing off between main() and variance_of_laplacian(). Frame image streamed includes only 1 channel whereas variance_of_laplacian() expects and image with 3 channels

--- blur_detection.py
+++ blur_detection.py
@@ -1,26 +1,61 @@
-# script for tuning parameters
-from imutils import paths
-import argparse
+"""
+Includes 2 of 3 functions needed to detect if input video is blurred
+"""
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from imutils import paths
 
-#code to determine how blurry something is-- has two arguments, path to input directory of
-#images and threshold for how blurry is really blurry
+CAM = "/Users/ErinTan/Projects/SMI_Pupillometry/SMI_Pupilometry_Test.mp4"
+cap = cv2.VideoCapture(CAM)
+
+def blur_output(bool_blur, int_blur):
+    """Prints green window for not blurred, red for blurred plus numeral"""
+     # Create black blank image
+    image = np.zeros((100, 100, 3), np.uint8)
+    if boolBlur:
+        # Fill image with green color
+        image[:] = (0, 255, 0)
+    else:
+        image[:] = (0, 0, 255)
+        print(intBlur)
+
+    cv2.imshow('light', image)
+    cv2.waitKey(0)
+
 def variance_of_laplacian(image):
+    """ code to determine how blurry something is """
+
 	# compute the Laplacian of the image and then return the focus
 	# measure, which is simply the variance of the Laplacian
-	return cv2.Laplacian(image, cv2.CV_64F).var()
-        threshold=100; #might need to change
-        #converts to gray
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        #variance of laplacian
-	fm = variance_of_laplacian(gray)
-	text = "Not Blurry"
+    threshold = 100
+
+	# might need to change
+    # converts to gray
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # variance of laplacian
+    fm = variance_of_laplacian(gray)
+    text = "Not Blurry"
+    flag = True
+
 	# if the focus measure is less than the supplied threshold,
 	# then the image should be considered "blurry"
-	if fm < threshold:
-		text = "Blurry"
+    if fm < threshold:
+        text = "Blurry"
+        flag = False
 	# show the image
-	cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
-	cv2.imshow("Image", image)
-	key = cv2.waitKey(0)
+    cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+    blur_output(flag, fm)
+    return cv2.Laplacian(image, cv2.CV_64F).var()
+
+if __name__ == "__main__":
+    while cap.isOpened():
+        ret, capture = cap.read()
+        frame = cv2.cvtColor(capture, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        variance_of_laplacian(frame)
+    cap.release()
