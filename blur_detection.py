fbcf983442cb174d53ed90e2778bb3ec2d528a4c
etan18 <erintan4@gmail.com>
2020-08-30 13:43:03 -0400

    Inserting main and delete open_stream.py

--- blur_detection.py
+++ blur_detection.py
@@ -61,3 +61,30 @@ cv2.imshow("Image", croppedImage)
 key = cv2.waitKey(0)
     #return bluriness
         #`how long does this take to do
+
+if __name__ == "__main__":
+    fvs = FileVideoStream(1).start()
+    time.sleep(1)
+
+    fps = FPS().start()
+
+    while fvs.more():
+        frame = fvs.read()
+        frame = imutils.resize(frame, width=450)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = np.dstack([frame, frame, frame])
+        blurriness = variance_of_laplacian(frame)
+
+        if blurry==1:
+            text="Blurry"
+        if blurry==0:
+            text="Not Blurry"
+        cv2.putText(gray, "{}: {:.2f}".format(text, bluriness), (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+        cv2.imshow("Frame", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        fps.update()
+
+    cv2.destroyAllWindows()
+    fvs.stop()       
