577f4250c13acd5246303207b7160854bfda3b89
aneeshakodati <68865291+aneeshakodati@users.noreply.github.com>
2020-08-31 13:46:08 -0400

    Variance_of_Laplacian returning two values

--- blur_detection.py
+++ blur_detection.py
@@ -4,39 +4,26 @@ import numpy
 from imutils.video import FPS
 from imutils.video import FileVideoStream
 
+Path = r'C:\Users\prashanthi\GitHub\Blur_Detection\HalfOpen_LookingAround.mp4'
 
-def variance_of_laplacian(image):
+def variance_of_laplacian(grayImage):
     Threshold=100
-
-#image = cv2.imread(path)
-
-        #start = time.time()
-
-
-        #end =time.time()
-        #print(end-start)
-    #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #height, width=image.shape
-    height=image.shape[0]
-    width=image.shape[1]
-    x=int(height/5)
-    y=int(width/5)
-    croppedImage = image[x:height-x, y:width-y]
-    small = cv2.resize(croppedImage, (0,0), fx=0.5, fy=0.5)
+    #height=grayImage.shape[0]
+    #width=grayImage.shape[1]
+    #x=int(height/5)
+    #y=int(width/5)
+    #croppedImage = grayImage[x:height-x, y:width-y]
+    small = cv2.resize(grayImage, (0,0), fx=0.5, fy=0.5)
     bluriness = cv2.Laplacian(small, cv2.CV_64F).var()
-    text= "Not Blurry "
+    blurry= False
     if bluriness < Threshold:
-        text="Blurry "
-    text+=str(bluriness)
-    return text
+        blurry= True
+    return blurry, bluriness
 
 
 if __name__ == "__main__":
     #path to video stream
-    #change
-    path = r'C:\Users\prashanthi\GitHub\Blur_Detection\HalfOpen_LookingAround.mp4'
-    stream = cv2.VideoCapture(path)
-    fvs = FileVideoStream(path).start()
+    fvs = FileVideoStream(Path).start()
     #time.sleep(1)
 
     fps = FPS().start()
@@ -45,12 +32,17 @@ if __name__ == "__main__":
 
         frame = fvs.read()
         if frame.all()!=None:
-            #frame = imutils.resize(frame, width=450)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            frame= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame = numpy.dstack([frame, frame, frame])
-        bluriness = variance_of_laplacian(frame)
-        cv2.putText(frame, "{}: {:.2f}".format(bluriness, 0), (10, 30),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+        boolean, bluriness = variance_of_laplacian(frame)
+        if boolean==False:
+            text="Not blurry, press q to quit\n"
+            cv2.putText(frame, "{}: {:.2f}".format(text, blurriness), (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 3)
+        else:
+            text="Blurry, press q to quit\n"
+            cv2.putText(frame, "{}: {:.2f}".format(text, blurriness), (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
         cv2.imshow("Frame", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -58,22 +50,3 @@ if __name__ == "__main__":
 
     cv2.destroyAllWindows()
     fvs.stop()
-
-#bluriness = cv2.Laplacian(gray, cv2.CV_64F).var()#second partial derivative of x/y direction
-#replacement for .var()?
-#resize+make threshold lower or crop out (u can control during data collectio)
-
-
-
-    #output number + boolean
-
-                # show the image
-#put text on after this function (return the stuff)
-
-    #cv2.putText(croppedImage, "{}: {:.2f}".format(text, bluriness), (10, 30),
-        #cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
-
-        #cv2.imshow("Image", croppedImage)
-        #key = cv2.waitKey(0)
-    #return bluriness
-        #`how long does this take to do
